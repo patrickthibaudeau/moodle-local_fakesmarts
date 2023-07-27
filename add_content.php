bbf1306d744cb5ec09dff5ff8e420183ba67e958
Patrick Thibaudeau <thibaud@yorku.ca>
2023-07-27 17:15:32 -0400

    Cleaned up file and added comments

--- add_content.php
+++ add_content.php
@@ -3,41 +3,7 @@
 require_once("../../config.php");
 
 require_once($CFG->dirroot . "/local/fakesmarts/classes/forms/add_content_form.php");
-/**
- * Loads all files found in a given folder.
- * Calls itself recursively for all sub folders.
- *
- * @param string $dir
- */
-function requireFilesOfFolder($dir)
-{
-    foreach (new DirectoryIterator($dir) as $fileInfo) {
-        if (!$fileInfo->isDot()) {
-            if ($fileInfo->isDir()) {
-                requireFilesOfFolder($fileInfo->getPathname());
-            } else {
-                require_once $fileInfo->getPathname();
-            }
-        }
-    }
-}
-
-$rootFolder = __DIR__.'/classes/Smalot/PdfParser';
-
-// Manually require files, which can't be loaded automatically that easily.
-require_once $rootFolder.'/Element.php';
-require_once $rootFolder.'/PDFObject.php';
-require_once $rootFolder.'/Font.php';
-require_once $rootFolder.'/Page.php';
-require_once $rootFolder.'/Element/ElementString.php';
-require_once $rootFolder.'/Encoding/AbstractEncoding.php';
-
-/*
- * Load the rest of PDFParser files from /src/Smalot/PDFParser
- * Dont worry, it wont load files multiple times.
- */
-requireFilesOfFolder($rootFolder);
-
+require_once('classes/rd_text_extraction.php');
 
 use local_fakesmarts\base;
 use local_fakesmarts\fakesmart_file;
@@ -46,47 +12,44 @@ use Smalot\PdfParser;
 
 global $CFG, $OUTPUT, $USER, $PAGE, $DB, $SITE;
 
-
 $context = CONTEXT_SYSTEM::instance();
 
 require_login(1, false);
-
+// Get bot id
 $bot_id= required_param('id', PARAM_INT);
 
 $formdata = new stdClass();
+// Set bot id in formdata
 $formdata->fakesmarts_id = $bot_id;
 
-
+// Create form
 $mform = new \local_fakesmarts\add_content_form(null, array('formdata' => $formdata));
 if ($mform->is_cancelled()) {
     //Handle form cancel operation, if cancel button is present on form
     redirect($CFG->wwwroot . '/local/fakesmarts/index.php');
 } else if ($data = $mform->get_data()) {
 
-    //common settings to all imports
+    // Get file name and content
     $filename = $mform->get_new_filename('importedFile');
     $fileContent = $mform->get_file_content('importedFile');
     $path = $CFG->dataroot . '/temp/fakesmarts';
 
+    // Does the folder exist for fakesmarts
     if(!is_dir($path)){
         mkdir($path);
     }
-
+    // Does the folder exist for the bot
     $path = $CFG->dataroot . '/temp/fakesmarts/' . $data->fakesmarts_id;
     if(!is_dir($path)){
         mkdir($path);
     }
-
+    // Set the file path with filename
     $file_path = $path . '/' . $filename;
-
+    // save the file
     file_put_contents($file_path, $fileContent);
-
-
-        require_once('classes/rd_text_extraction.php');
-        $content = RD_Text_Extraction::convert_to_text($file_path);
-
-
-
+    // Convert the content of the file to text
+    $content = RD_Text_Extraction::convert_to_text($file_path);
+    // Create content data array
     $content_data = [
         'fakesmarts_id' => $data->fakesmarts_id,
         'content' => $content,
@@ -95,13 +58,12 @@ if ($mform->is_cancelled()) {
         'timemodified' => time(),
         'timecreated' => time(),
     ];
-
+    // Insert the content into the database
     $DB->insert_record('local_fakesmarts_files', $content_data);
-
+    // Redirect to the content page
     redirect($CFG->wwwroot . '/local/fakesmarts/content.php?id=' . $data->fakesmarts_id,);
-
 } else {
-
+    // Show form
     $mform->set_data($mform);
 }
 
@@ -114,14 +76,12 @@ base::page(
     'standard'
 );
 
-
 echo $OUTPUT->header();
 //**********************
 //*** DISPLAY HEADER ***
 //
 
 $mform->display();
-
 
 //**********************
 //*** DISPLAY FOOTER ***
